573bd985db158c62a4b3311343e95a209d93e1ab
Vallabh Kansagara <vrkansagara@gmail.com>
2018-01-16 22:51:57 +0530

    adding required file(s).
    Signed-off-by: Vallabh Kansagara <vrkansagara@gmail.com>

---
+++ test/CommonCrawler/EntryEntityTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CommonCrawl;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class EntryEntityTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->entry = new EntryEntity();
+    }
+}
